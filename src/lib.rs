68449ac61934b57d7bab44c0202980c5af4bb737
Scott Atkins <scottrick49@gmail.com>
2022-04-13 17:20:02 -0500

    Added Vec3 and Sphere.

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+}
+
+#[derive(Debug)]
+pub struct Sphere {
+    pub center: Vec3,
+    pub radius: f32,
+}
+
+impl Sphere {
+    pub fn new(center: Vec3, radius: f32) -> Self {
+        Self { center, radius }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rust_rt::{Sphere, Vec3};
+
 fn main() {
     println!("Hello, rust_rt!");
+
+    let center_point = Vec3::new(49., 49., 52.);
+    let sphere = Sphere::new(center_point, 7.0);
+
+    println!("point: {:?}", sphere.center);
+    println!("sphere: {:?}", sphere);
 }
